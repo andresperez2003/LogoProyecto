d7efbd628140d778c1b75102f1886b9c6c466527
Andres <andresap2017@gmail.com>
2023-05-14 20:24:59 -0500

    Relacion realizada de el lector hacia el controlador

--- src/co/edu/autonoma/classLogo/LectorinstruccionesTortuga.java
+++ src/co/edu/autonoma/classLogo/LectorinstruccionesTortuga.java
@@ -3,19 +3,58 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package co.edu.autonoma.classLogo;
-
+ 
 /**
  *
  * @author ASUS
  */
 public class LectorinstruccionesTortuga {
+    private Controlador controlador;
     
-    
-    public void read(){
-        
+    public boolean read(String movimiento) {
+        String[] division = movimiento.split(" ");
+        String instruccion = division[0].toLowerCase();
+        switch (instruccion) {
+            case "fd", "forward" -> {
+                controlador.forward();
+            }
+            case "bd", "backward" -> {
+                controlador.backward();
+            }
+            case "rt", "rigthturn" -> {
+                controlador.rigthTurn();
+            }
+            case "ld", "leftturn" -> {
+                controlador.leftTurn();
+            }
+            case "sc", "setcolor" -> {
+                controlador.setColor();
+            }
+            case "r", "reset" -> {
+                controlador.reset();
+            }
+            case "h", "home" -> {
+                controlador.home();
+            }
+            case "l", "load" -> {
+                controlador.load();
+            }
+            case "s", "save" -> {
+                controlador.save();
+            }
+            default -> {
+                return false;
+            }
+        }
+        return true;
     }
+
+    public LectorinstruccionesTortuga() {
+    }
+
     
-    public String returnMove(){
+
+    public String returnMove() {
         return "";
     }
 }
